90b24f5238d308f179d30225108f5acf1b4315ba
Rubinschwein47 <felixconrad7@gmail.com>
2025-04-08 11:15:59 +0200

    tests do be testing

--- src/test/kotlin/com/safedo/converter/converter/JsonToXmlConverterTest.kt
+++ src/test/kotlin/com/safedo/converter/converter/JsonToXmlConverterTest.kt
@@ -1,22 +1,48 @@
 package com.safedo.converter.converter
 
 import io.quarkus.test.junit.QuarkusTest
-import netscape.javascript.JSObject
+import jakarta.inject.Inject
 import org.json.JSONObject
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import javax.inject.Inject
 
 @QuarkusTest
-public class JsonToXmlConverterTest {
+class JsonToXmlConverterTest {
     @Inject
     lateinit var converter: JsonToXmlConverter
+    var expectedObject = JSONObject()
+        .put("testA", 123)
+        .put("testB", "Hello World")
+        .put(
+            "testC", JSONObject()
+                .put("first", "some")
+                .put("second", "value")
+        )
+
+    var JsonString = "{\n" +
+            "  \"testA\": 123,\n" +
+            "  \"testB\": \"Hello World\",\n" +
+            "  \"testC\": {\n" +
+            "    \"first\": \"some\",\n" +
+            "    \"second\": \"value\"\n" +
+            "  }\n" +
+            "}"
+    var XmlString =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><testB>Hello World</testB><testC><first>some</first><second>value</second></testC><testA>123</testA>"
+
 
     @Test
     fun testToObject() {
-        var expectedObject= JSONObject()
-            .put("testA",123)
-            .put("testB","Hello World")
-        assertEquals()
+        assertEquals(converter.toObject(JsonString).toString(), expectedObject.toString())
+    }
+
+    @Test
+    fun testToString() {
+        assertEquals(converter.toString(expectedObject), XmlString)
+    }
+
+    @Test
+    fun testConvert() {
+        assertEquals(converter.convert(JsonString), XmlString)
     }
 }
