ce81ed91fb759258065678f17f370585bde85b7e
DotNomi <dommiii125@gmail.com>
2025-04-09 10:28:39 +0200

    corrected linting

--- src/main/kotlin/com/safedo/converter/ConverterResource.kt
+++ src/main/kotlin/com/safedo/converter/ConverterResource.kt
@@ -11,20 +11,18 @@ import jakarta.ws.rs.core.Response
 
 @Path("/v1/converter")
 class ConverterResource(
-    private val xmlToJsonConverter: XmlToJsonConverter, private val jsonToXmlConverter: JsonToXmlConverter
+    private val xmlToJsonConverter: XmlToJsonConverter,
+    private val jsonToXmlConverter: JsonToXmlConverter
 ) {
     @POST
     @Path("/xml/{outFormat}")
     @Consumes(MediaType.APPLICATION_XML)
-    fun formatXml(
-        @PathParam("outFormat") outFormat: String, body: String
-    ): Response {
+    fun formatXml(@PathParam("outFormat") outFormat: String, body: String): Response {
         return when (outFormat.lowercase()) {
             "json" -> {
                 Response.ok().header("Content-Type", MediaType.APPLICATION_JSON)
                     .entity(xmlToJsonConverter.convert(body)).build()
             }
-
             else -> Response.status(Response.Status.BAD_REQUEST).build()
         }
     }
@@ -32,15 +30,12 @@ class ConverterResource(
     @POST
     @Path("/json/{outFormat}")
     @Consumes(MediaType.APPLICATION_JSON)
-    fun formatJson(
-        @PathParam("outFormat") outFormat: String, body: String
-    ): Response {
+    fun formatJson(@PathParam("outFormat") outFormat: String, body: String): Response {
         return when (outFormat.lowercase()) {
             "xml" -> {
                 Response.ok().header("Content-Type", MediaType.APPLICATION_XML)
                     .entity(jsonToXmlConverter.convert(body)).build()
             }
-
             else -> Response.status(Response.Status.BAD_REQUEST).build()
         }
     }
